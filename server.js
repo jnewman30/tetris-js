d3a2db672e9bcf0b82782de2079e0d446dcf65b6
CaptainZod <jnewman30@gmail.com>
2019-05-19 10:37:13 -0500

    move index.html to root of project

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express'),
 server.set('port', process.env.PORT || 3000);
 
 server.use(express.static('app'));
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 server.listen(3000, () => {
     console.log('Express server started at port 3000');
